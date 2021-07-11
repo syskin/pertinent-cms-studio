c47b45977757949d8c74cbe47ed16492b42e0713
syskin <mcadu@yahoo.fr>
2021-07-11 16:33:16 +0200

    feat: add menu links

--- components/interface/menu/MenuHeader.tsx
+++ components/interface/menu/MenuHeader.tsx
@@ -1,13 +1,45 @@
 
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store'
+import Link from 'next/link'
 
 const MenuHeader = () => {
     const { state } = useSelector((state: RootState) => state.sidebar)
-    const baseMenuHeaderWrapper = `transform duration-300 bg-gray-100 w-full h-12 fixed top-0 z-10 mb-12`
+    const baseMenuHeaderWrapper = `transform duration-300 bg-gray-100 w-full h-12 fixed top-0 z-10`
+
+    type Links = Link[]
+
+    interface Link {
+        uri: string,
+        text: string,
+    }
+
+    const linksMenu: Links = [
+        {
+            uri: '/',
+            text: 'Your Website home page'
+        },
+        {
+            uri: '/studio',
+            text: 'Studio'
+        }
+    ]
+
     return (
         <div className={state ? `${baseMenuHeaderWrapper} pr-72` : `${baseMenuHeaderWrapper}`}>
-            My menu
+            <ul className="flex flex-row justify-start items-center h-full">
+            {
+                linksMenu.map((link: Link, index: number) => {
+                    return (
+                        <li className="mx-2" key={`menu_link_${index}`}>
+                            <Link href={link.uri}>
+                                <a>{link.text}</a>
+                            </Link>
+                        </li>
+                    )
+                })
+            }
+            </ul>
         </div>
     )
 }
