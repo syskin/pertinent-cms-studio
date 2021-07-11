c16d8bc9a2b4defae00d2fea7aa41a6e4934280c
syskin <mcadu@yahoo.fr>
2021-07-11 17:08:41 +0200

    fix: lint issues and husky precommit lint cmd

--- components/interface/menu/MenuHeader.tsx
+++ components/interface/menu/MenuHeader.tsx
@@ -1,47 +1,44 @@
-
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store'
 import Link from 'next/link'
 
 const MenuHeader = () => {
-    const { state } = useSelector((state: RootState) => state.sidebar)
-    const baseMenuHeaderWrapper = `transform duration-300 bg-gray-100 w-full h-12 fixed top-0 z-10`
+  const { state } = useSelector((state: RootState) => state.sidebar)
+  const baseMenuHeaderWrapper = `transform duration-300 bg-gray-100 w-full h-12 fixed top-0 z-10`
 
-    type Links = Link[]
+  type Links = Link[]
 
-    interface Link {
-        uri: string,
-        text: string,
-    }
+  interface Link {
+    uri: string
+    text: string
+  }
 
-    const linksMenu: Links = [
-        {
-            uri: '/',
-            text: 'Your Website home page'
-        },
-        {
-            uri: '/studio',
-            text: 'Studio'
-        }
-    ]
+  const linksMenu: Links = [
+    {
+      uri: '/',
+      text: 'Your Website home page',
+    },
+    {
+      uri: '/studio',
+      text: 'Studio',
+    },
+  ]
 
-    return (
-        <div className={state ? `${baseMenuHeaderWrapper} pr-72` : `${baseMenuHeaderWrapper}`}>
-            <ul className="flex flex-row justify-start items-center h-full">
-            {
-                linksMenu.map((link: Link, index: number) => {
-                    return (
-                        <li className="mx-2" key={`menu_link_${index}`}>
-                            <Link href={link.uri}>
-                                <a>{link.text}</a>
-                            </Link>
-                        </li>
-                    )
-                })
-            }
-            </ul>
-        </div>
-    )
+  return (
+    <div className={state ? `${baseMenuHeaderWrapper} pr-72` : `${baseMenuHeaderWrapper}`}>
+      <ul className="flex flex-row justify-start items-center h-full">
+        {linksMenu.map((link: Link, index: number) => {
+          return (
+            <li className="mx-2" key={`menu_link_${index}`}>
+              <Link href={link.uri}>
+                <a>{link.text}</a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default MenuHeader
