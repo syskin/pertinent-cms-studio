5a5ec8833fd0ae42cec50f09c218cb32af7eed69
syskin <mcadu@yahoo.fr>
2021-07-16 21:52:13 +0200

    Design page item

--- components/pages/PageItem/index.tsx
+++ components/pages/PageItem/index.tsx
@@ -8,9 +8,10 @@ interface PagesItemProps {
 const MenuHeader: React.FC<PagesItemProps> = ({ page }) => {
   return (
     <div className="flex flex-row flex-no-wrap rounded-lg bg-gray-100 px-4 py-2 shadow-md hover:shadow-lg transform duration-300 my-4">
-      <div className="flex flex-col w-4/5">
+      <div className="flex flex-col w-4/5 px-2">
         <span>{page.name}</span>
-        <p>{page.description}</p>
+        <span className="text-xs text-gray-500">{page.path}</span>
+        <p className="text-sm text-gray-600 truncate mt-2">{page.description}</p>
       </div>
       <div className="flex flex-col w-1/5">
         <Link href={`/studio/page/${page.id}`}>
