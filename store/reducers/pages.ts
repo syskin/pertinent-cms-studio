8c50d1d9ef76dd00409f97266115d24a0a02c925
syskin <mcadu@yahoo.fr>
2021-07-20 18:41:20 +0200

    Add reducers type

--- store/reducers/pages.ts
+++ store/reducers/pages.ts
@@ -1,4 +1,11 @@
-import { DefaultState } from '../types/pages'
+import {
+  DefaultAction,
+  DefaultState,
+  DELETE_ONE_BY_ID,
+  GET_ALL,
+  GET_ONE_BY_ID,
+  UPDATE_ONE_BY_ID,
+} from '../types/pages'
 
 const initialState: DefaultState = {
   activePage: {
@@ -6,12 +13,33 @@ const initialState: DefaultState = {
     id: '',
     slug: '',
     description: '',
+    root: [],
   },
   pages: [],
 }
 
-const pagesReducer = (state = initialState): DefaultState => {
-  return state
+const pagesReducer = (state = initialState, action: DefaultAction): DefaultState => {
+  if (!action || !action.type) return state
+  switch (action.type) {
+    case GET_ALL:
+      return {
+        ...state,
+      }
+    case GET_ONE_BY_ID:
+      return {
+        ...state,
+      }
+    case UPDATE_ONE_BY_ID:
+      return {
+        ...state,
+      }
+    case DELETE_ONE_BY_ID:
+      return {
+        ...state,
+      }
+    default:
+      return state
+  }
 }
 
 export default pagesReducer
--- store/types/pages.ts
+++ store/types/pages.ts
@@ -1,6 +1,32 @@
 import { Page } from '../../types/pages'
 
+export const GET_ALL = 'GET_ALL_PAGES'
+export const GET_ONE_BY_ID = 'GET_ONE_PAGE_BY_ID'
+export const UPDATE_ONE_BY_ID = 'UPDATE_ONE_PAGE_BY_ID'
+export const DELETE_ONE_BY_ID = 'DELETE_ONE_PAGE_BY_ID'
+
 export interface DefaultState {
   pages: Page[]
   activePage: Page
 }
+
+interface getAllPages {
+  type: typeof GET_ALL
+}
+
+interface getOnePageById {
+  type: typeof GET_ONE_BY_ID
+  id: string
+}
+
+interface updateOnePageById {
+  type: typeof UPDATE_ONE_BY_ID
+  page: Page
+}
+
+interface deleteOnePageById {
+  type: typeof DELETE_ONE_BY_ID
+  id: string
+}
+
+export type DefaultAction = getAllPages | getOnePageById | updateOnePageById | deleteOnePageById
